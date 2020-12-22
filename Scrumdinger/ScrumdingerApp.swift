4649f3cce525fb248548e43b581f0c6c95616386
Shawn Roller <sroller@techstyle.com>
2020-12-21 21:02:39 -0500

    initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Shawn Roller on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
