c22ef2e7963393f33f2f82ee82fb23a8f45db77f
Shawn Roller <sroller@techstyle.com>
2021-01-23 15:46:15 -0500

    Refactors circle to MeetingTimerView.

---
+++ Scrumdinger/MeetingTimerView.swift
@@ -0,0 +1,36 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Shawn Roller on 1/23/21.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    @Binding var speakers: [ScrumTimer.Speaker]
+    var scrumColor: Color
+    private var currentSpeaker: String { speakers.first(where: { !$0.isCompleted })?.name ?? "Someone" }
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            VStack {
+                Text(currentSpeaker)
+                    .font(.title)
+                Text("is speaking")
+            }
+            .accessibilityElement(children: .combine)
+            .foregroundColor(scrumColor.accessibleFontColor)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    @State static var speakers = [ScrumTimer.Speaker(name: "Shane", isCompleted: true), ScrumTimer.Speaker(name: "Landsea", isCompleted: false)]
+    static var previews: some View {
+        MeetingTimerView(speakers: $speakers, scrumColor: Color("Design"))
+    }
+}
